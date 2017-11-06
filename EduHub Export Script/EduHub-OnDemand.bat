f430a7259291103080c4f0b346b0626e46e48378
Robert Brandon <brandon.robert.b@edumail.vic.gov.au>
2017-11-06 15:49:04 +1100

    Add new file

---
+++ EduHub Export Script/EduHub-OnDemand.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+
+if exist L:\ (
+    ECHO Removing existing L:\...
+    Net use L: /delete
+)
+
+ECHO Mapping eduHub share to L:\...
+Net use L: \\10.0.0.1\eduHub$ Password /User:DOMAIN\eduhubUser
+
+ECHO Running Import Script...
+PowerShell.exe -ExecutionPolicy Bypass -Command "& '%~dpn0.ps1'"
+
+ECHO Removing eduHub share...
+Net use L: /delete
+
+ECHO Done.
+timeout /t 5
